34558670472f7345ac229705bf5f15cecaab18cc
azimhatami <azimhatami4@gmail.com>
2024-10-07 00:43:25 +0330

    Create header and footer components

--- courses_list/src/App.jsx
+++ courses_list/src/App.jsx
@@ -1,12 +1,15 @@
 import CourseList from './components/CourseList';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import './App.css'
 
 function App() {
 
   return (
     <>
-		<h1>My Courses (3)</h1>
-		<CourseList />
+	  <Header />
+	  <CourseList />
+	  <Footer />
     </>
   )
 }
---
+++ courses_list/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+	
+	return(
+		<>
+			<div className='footer'>
+				You have seccessfully passed <strong>33%</strong> of your courses.
+			</div>
+		</>
+	);
+}
+
+
+export default Footer;
