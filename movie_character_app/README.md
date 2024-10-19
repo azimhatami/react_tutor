4fee49e8011bd24336071e1bd52f7bc8106a89ed
azimhatami <azimhatami4@gmail.com>
2024-10-19 11:24:30 +0330

    Initial commit

---
+++ movie_character_app/README.md
@@ -0,0 +1,10 @@
+# Movie Character App
+
+- Rick and Morty API is used in this project.
+
+
+```bash
+npm install
+npm run dev
+```
+
---
+++ movie_character_app/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2>Movie Character App</h2>
+    </>
+  )
+}
+
+export default App
