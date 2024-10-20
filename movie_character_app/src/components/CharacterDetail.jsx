599b5810808d2f96215e62d4ee236448b6180eda
azimhatami <azimhatami4@gmail.com>
2024-10-20 12:10:32 +0330

    Refactor CharacterDetail with fake data

--- movie_character_app/src/components/CharacterDetail.jsx
+++ movie_character_app/src/components/CharacterDetail.jsx
@@ -1,29 +1,32 @@
 import { ArrowUpCircleIcon } from '@heroicons/react/24/outline';
+import { IoManSharp, IoWoman} from 'react-icons/io5';
+import { character } from '../../data/data';
+import { episodes } from '../../data/data';
 
 function CharacterDetail() {
   return (
     <>
       <div className='character_details'>
         <img 
-        src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" 
-        alt="Rick Sanchez" 
+        src={character.image} 
+        alt={character.name} 
         className="character_detail_img"
         />
         <div className='character_details_info'>
           <div className='character_details_info_header'>
             <h3 className="name">
-              <span>👱🏻‍♂️</span>
+              <span>{character.gender === 'Male' ? <IoManSharp className='man' /> : <IoWoman className='woman' />}</span>
               <span> Rick Sanchez</span>
             </h3>
             <div className='info'>
-              <span className="status"></span>
-              <span> Alive </span>
-              <span> - Human</span>
+              <span className={`status ${character.status === 'Dead' ? 'red' : ''}`}></span>
+              <span> { character.status } </span>
+              <span> - {character.species}</span>
             </div>
           </div>
           <div className="location">
             <p>Last known location:</p>
-            <p>Citadel of Ricks</p>
+            <p>{character.location.name}</p>
           </div>
           <div className="actions">
             <button className="btn btn_primary">Add to Favourite</button>
@@ -38,30 +41,12 @@ function CharacterDetail() {
           </button>
         </div>
         <ul>
-          <li>
-            <div>01 - S01E01 : <strong>Pilot</strong></div>
-            <div className="badge badge_secondary">December 2, 2013</div>
-          </li>
-          <li>
-            <div>02 - S01E02 : <strong>Lawnmower Dog</strong></div>
-            <div className="badge badge_secondary">December 9, 2013</div>
-          </li>
-          <li>
-            <div>03 - S01E03 : <strong>Anatomy Park</strong></div>
-            <div className="badge badge_secondary">December 16, 2013</div>
-          </li>
-          <li>
-            <div>04 - S01E04 : <strong>M. Night Shaym-Aliens!</strong></div>
-            <div className="badge badge_secondary">January 13, 2014</div>
-          </li>
-          <li>
-            <div>05 - S01E05 : <strong>Meeseeks and Destroy</strong></div>
-            <div className="badge badge_secondary">January 20, 2014</div>
-          </li>
-          <li>
-            <div>06 - S01E06 : <strong>Rick Potion #9</strong></div>
-            <div className="badge badge_secondary">January 27, 2014</div>
-          </li>
+          {episodes.map((item, index) => 
+            <li key={item.id}>
+              <div>{String(index + 1).padStart(2, '0')} - {item.episode} : <strong>{item.name}</strong></div>
+              <div className="badge badge_secondary">{item.air_date}</div>
+            </li>
+          )}
         </ul>
       </div>
     </>
