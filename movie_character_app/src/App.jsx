b97e7158fe27d68c517925fec9604309ddf5760c
azimhatami <azimhatami4@gmail.com>
2024-10-20 11:08:54 +0330

    Refactor CharacterList component with fake data

--- movie_character_app/src/App.jsx
+++ movie_character_app/src/App.jsx
@@ -2,6 +2,7 @@ import { EyeIcon } from '@heroicons/react/24/outline';
 import Navbar from './components/Navbar';
 import CharacterList from './components/CharacterList';
 import CharacterDetail from './components/CharacterDetail';
+import { allCharacters } from '../data/data';
 import { useState } from 'react'
 import './App.css'
 
@@ -12,7 +13,7 @@ function App() {
       <div className='app'>
         <Navbar />
         <div className='sidbar'>
-          <CharacterList />
+          <CharacterList characters={ allCharacters }/>
         </div>
         <div className='main'>
           <CharacterDetail />
--- movie_character_app/src/components/CharacterList.jsx
+++ movie_character_app/src/components/CharacterList.jsx
@@ -1,65 +1,41 @@
 import { EyeIcon } from '@heroicons/react/24/outline';
+import { IoManSharp } from "react-icons/io5";
+import { IoWoman } from "react-icons/io5";
 
-function CharacterList() {
+function CharacterList({characters}) {
   return (
     <div className='characters-list'>
-      <div className='list_item'>
-        <img 
-        src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" 
-        alt="Rick Sanchez"
-        />
-        <h3 className="name">
-          <span>👱🏻‍♂️</span>
-          <span> Rick Sanchez</span>
-        </h3>
-        <div className="list_item_info info">
-          <span className="status"></span>
-          <span> Alive </span>
-          <span> - Human</span>
-        </div>
-        <button className="icon red">
-          <EyeIcon />
-        </button>
-      </div>
-      <div className='list_item'>
-        <img 
-        src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" 
-        alt="Rick Sanchez"
-        />
-        <h3 className="name">
-          <span>👱🏻‍♂️</span>
-          <span> Rick Sanchez</span>
-        </h3>
-        <div className="list_item_info info">
-          <span className="status"></span>
-          <span> Alive </span>
-          <span> - Human</span>
-        </div>
-        <button className="icon red">
-          <EyeIcon />
-        </button>
-      </div>
-      <div className='list_item'>
-        <img 
-        src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" 
-        alt="Rick Sanchez"
-        />
-        <h3 className="name">
-          <span>👱🏻‍♂️</span>
-          <span> Rick Sanchez</span>
-        </h3>
-        <div className="list_item_info info">
-          <span className="status"></span>
-          <span> Alive </span>
-          <span> - Human</span>
-        </div>
-        <button className="icon red">
-          <EyeIcon />
-        </button>
-      </div>
+      {characters.map((item) => <Character key={item.id} item={item} />)} 
     </div>
   );
 }
 
 
 export default CharacterList;
+
+
+function Character({item}) {
+  return (
+    <>
+    <div className='list_item'>
+      <img 
+      src={item.image} 
+      alt={item.image}
+      />
+      <h3 className="name">
+        <span>{item.gender === 'Male' ? <IoManSharp className='man' /> : <IoWoman className='woman' />}</span>
+        <span> {item.name}</span>
+      </h3>
+      <div className="list_item_info info">
+        <span className={`status ${item.status === 'Dead' ? 'red' : ''}`}></span>
+        <span> { item.status } </span>
+        <span> - { item.species }</span>
+      </div>
+      <button className="icon red">
+        <EyeIcon />
+      </button>
+    </div>
+    </>
+  );
+}
+
