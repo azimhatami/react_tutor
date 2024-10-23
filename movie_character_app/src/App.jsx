b03aea1906c0ce2bc941b710adf3b8e3e5fde768
azimhatami <azimhatami4@gmail.com>
2024-10-23 18:46:31 +0000

    Create loading component

--- movie_character_app/src/App.jsx
+++ movie_character_app/src/App.jsx
@@ -2,6 +2,7 @@ import { EyeIcon } from '@heroicons/react/24/outline';
 import Navbar from './components/Navbar';
 import CharacterList from './components/CharacterList';
 import CharacterDetail from './components/CharacterDetail';
+import Loader from './components/Loader';
 import { allCharacters } from '../data/data';
 import { useState, useEffect } from 'react'
 import './App.css'
@@ -9,6 +10,7 @@ import './App.css'
 function App() {
 
   const [characters, setCharacters] = useState(allCharacters);
+  const [isLoading, setIsLoading] = useState(false);
 
   // Not to fetch in this way:
   // fetch('https://rickandmortyapi.com/api/character').then((res) => res.json()).
@@ -16,9 +18,11 @@ function App() {
 
   useEffect(() => {
     async function fetchData() {
+      setIsLoading(true)
       const response = await fetch('https://rickandmortyapi.com/api/character');
       const data = await response.json();
       setCharacters(data.results)
+      setIsLoading(false)
     }
 
     fetchData()
@@ -33,7 +37,7 @@ function App() {
       <div className='app'>
         <Navbar numOfSearch={characters.length} />
         <div className='sidbar'>
-          <CharacterList characters={ characters }/>
+          { isLoading ? <Loader/> : <CharacterList characters={ characters }/>}
         </div>
         <div className='main'>
           <CharacterDetail />
