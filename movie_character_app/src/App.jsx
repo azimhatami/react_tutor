0d5f7228d8d08b32ffdae79d97dd122266a9ffda
azimhatami <azimhatami4@gmail.com>
2024-10-24 12:39:23 +0330

    Error handling and usage toast for error

--- movie_character_app/src/App.jsx
+++ movie_character_app/src/App.jsx
@@ -5,11 +5,12 @@ import CharacterDetail from './components/CharacterDetail';
 import Loader from './components/Loader';
 import { allCharacters } from '../data/data';
 import { useState, useEffect } from 'react'
+import toast, { Toaster } from 'react-hot-toast';
 import './App.css'
 
 function App() {
 
-  const [characters, setCharacters] = useState(allCharacters);
+  const [characters, setCharacters] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
   // Not to fetch in this way:
@@ -18,23 +19,49 @@ function App() {
 
   useEffect(() => {
     async function fetchData() {
-      setIsLoading(true)
-      const response = await fetch('https://rickandmortyapi.com/api/character');
-      const data = await response.json();
-      setCharacters(data.results)
-      setIsLoading(false)
+      try {
+        setIsLoading(true)
+        const response = await fetch('https://rickandmortyapi.com/api/character');
+
+        if (!response.ok) throw new Error('Something went wrong!');
+
+        const data = await response.json();
+        setCharacters(data.results)
+        // setIsLoading(false)
+      } catch(error) {
+        // setIsLoading(false)
+        // FOR REAL PROJECT -> error.response.data.message
+        // console.log(error.message)
+        toast.error(error.message)
+      } finally {
+        setIsLoading(false)
+      }
     }
-
     fetchData()
-
-    // fetch('https://rickandmortyapi.com/api/character').
-      // then((response) => response.json()).
-      // then((data) => setCharacters(data.results))
   }, [])
+
+
+//  useEffect(() => {
+//    setIsLoading(true)
+//    fetch('https://rickandmortyapi.com/api/character').
+//      then((response) => {
+//        if (!response.ok) throw new Error('Somthing went wrong!')
+//        return response.json()
+//      }).
+//      then((data) => {
+//        setCharacters(data.results)
+//        // setIsLoading(false)
+//      }).
+//      catch((error) => {
+//        // setIsLoading(false)
+//        toast.error(error.message)
+//      }).finally(() => setIsLoading(false))
+//  }, [])
 
   return (
     <>
       <div className='app'>
+        <Toaster/>
         <Navbar numOfSearch={characters.length} />
         <div className='sidbar'>
           { isLoading ? <Loader/> : <CharacterList characters={ characters }/>}
