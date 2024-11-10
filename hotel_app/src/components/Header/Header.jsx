468b6db98015171659aabdd98b5fe387f43775a5
azimhatami <azimhatami4@gmail.com>
2024-11-11 00:10:26 +0330

    Add tallwind css for style

---
+++ hotel_app/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return(
+    <>
+      <div 
+        className='border-solid border-2 border-zinc-300 rounded-md p-4'
+      >
+        Header
+      </div>
+    </>
+  );
+}
+
+export default Header;
