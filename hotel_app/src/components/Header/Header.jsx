b890583f22ddd47a68ed9242535657fcaa424269
azimhatami <azimhatami4@gmail.com>
2024-11-13 16:54:42 +0000

    Search options drop down completed

--- hotel_app/src/components/Header/Header.jsx
+++ hotel_app/src/components/Header/Header.jsx
@@ -3,8 +3,9 @@ import { HiCalendar } from "react-icons/hi";
 import { IoSearch } from "react-icons/io5";
 import { MdLocationPin } from "react-icons/md";
 
-function Header() {
-  const [destination, setDestination] = useState('');
+export function Header() {
+  const [destination, setDestination] = useState("");
+  const [openOptions, setOpenOptions] = useState(false);
   return (
     <>
       <div className="border-solid border-[1px] border-slate-400 rounded-md py-4">
@@ -21,16 +22,19 @@ function Header() {
           </div>
           <span className="w-[1px] h-7 bg-slate-400"></span>
           <div className="flex items-center justify-between w-[7rem]">
-            <HiCalendar className="text-purple-700 text-2xl"/>
+            <HiCalendar className="text-purple-700 text-2xl" />
             <div>2024/05/11</div>
           </div>
           <span className="w-[1px] h-7 bg-slate-400"></span>
           <div>
-            <div>1 adult - 2 children - 1 room</div>
+            <div className="" onClick={() => setOpenOptions(!openOptions)}>
+              1 adult - 2 children - 1 room
+            </div>
+            {openOptions && <GuestOptionList />}
           </div>
           <div>
             <button className="bg-purple-700 text-slate-300 w-10 h-10 rounded-full text-2xl flex items-center">
-              <IoSearch className="flex m-auto"/>
+              <IoSearch className="flex m-auto" />
             </button>
           </div>
         </div>
@@ -39,4 +43,47 @@ function Header() {
   );
 }
 
-export default Header;
+export function GuestOptionList() {
+  return (
+    <>
+      <div className="w-56 h-30 bg-slate-200 rounded-lg absolute top-[90px] drop-shadow-xl transition-all ease-in-out duration-700 delay-700 py-2 flex flex-col gap-2">
+        <div className="flex align-center justify-around">
+          <span>Adult</span>
+          <div className="flex align-center justify-around w-28">
+            <button className="bg-purple-700 text-slate-50 w-8 rounded-lg">
+              -
+            </button>
+            <span>4</span>
+            <button className="bg-purple-700 text-slate-50 w-8 rounded-lg">
+              +
+            </button>
+          </div>
+        </div>
+        <div className="flex align-center justify-around">
+          <span>Children</span>
+          <div className="flex align-center justify-around w-28">
+            <button className="bg-purple-700 text-slate-50 w-8 rounded-lg">
+              -
+            </button>
+            <span>4</span>
+            <button className="bg-purple-700 text-slate-50 w-8 rounded-lg">
+              +
+            </button>
+          </div>
+        </div>
+        <div className="flex align-center justify-around">
+          <span>Room</span>
+          <div className="flex align-center justify-around w-28">
+            <button className="bg-purple-700 text-slate-50 w-8 rounded-lg">
+              -
+            </button>
+            <span>4</span>
+            <button className="bg-purple-700 text-slate-50 w-8 rounded-lg">
+              +
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
