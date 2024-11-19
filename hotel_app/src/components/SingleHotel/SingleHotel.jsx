de27a68cb27ed24530175c0b94ac1c3a5f4f9d40
azimhatami <azimhatami4@gmail.com>
2024-11-19 18:22:23 +0330

    Single hotel view

--- hotel_app/src/components/SingleHotel/SingleHotel.jsx
+++ hotel_app/src/components/SingleHotel/SingleHotel.jsx
@@ -1,7 +1,25 @@
+import { useParams } from 'react-router-dom';
+import useFetch from '../../hooks/useFetch';
+
 function SingleHotel() {
+  const { id } = useParams();
+  const { data, isLoading } = useFetch(`http://localhost:3000/hotels/${id}`);
+
+  if (isLoading) return <p>Loading ...</p>
+
   return(
     <>
-      <h2>Single Hotel</h2>
+      <div className='flex flex-col items-start mt-8 ml-10'>
+        <img 
+          src={data.xl_picture_url} 
+          alt={data.name} 
+          className='w-[24rem] rounded-lg'
+        />
+        <h2 className='text-[1.2rem] font-bold mt-4'>{data.name}</h2>
+        <div className='text-slate-500 font-medium'>
+          {data.number_of_reviews} reviews &bull; {data.smart_location} 
+        </div>
+      </div>
     </>
   );
 }
