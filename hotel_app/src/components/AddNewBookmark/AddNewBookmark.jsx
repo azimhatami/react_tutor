e2b2504c1b3e4dc38d06010f16aef44ad18e63e6
azimhatami <azimhatami4@gmail.com>
2024-11-23 00:34:08 +0330

    Fetch location data

--- hotel_app/src/components/AddNewBookmark/AddNewBookmark.jsx
+++ hotel_app/src/components/AddNewBookmark/AddNewBookmark.jsx
@@ -1,9 +1,56 @@
 import { useNavigate } from "react-router-dom";
 import useUrlLocation from "../../hooks/useUrlLocation";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function getFlagEmoji(countryCode) {
+    const codePoints = countryCode
+      .toUpperCase()
+      .split('')
+      .map(char =>  127397 + char.charCodeAt());
+    return String.fromCodePoint(...codePoints);
+  }
+
+const BASE_GEOCODING_URL = "https://api-bdc.net/data/reverse-geocode-client";
 
 function AddNewBookmark() {
-    const navigate = useNavigate();
-    const [lat, lng] = useUrlLocation();
+  const navigate = useNavigate();
+  const [lat, lng] = useUrlLocation();
+  const [cityName, setCityName] = useState();
+  const [country, setCountry] = useState();
+  const [countryCode, setCountryCode] = useState("");
+  const [isLoadingGeoCoding, setIsLoadingGeoCoding] = useState(false);
+  const [geoCodingError, setGeoCodingError] = useState(null);
+
+  useEffect(() => {
+    if (!lat || !lng) return;
+    async function fetchLocationData() {
+      setIsLoadingGeoCoding(true);
+      setGeoCodingError(null);
+      try {
+        const { data } = await axios.get(
+          `${BASE_GEOCODING_URL}?latitude=${lat}&longitude=${lng}`
+        );
+
+        if (!data.countryCode) throw new Error('This location is not a city! please click somewhere else.')
+
+        console.log(data);
+        setCityName(data.city || data.locality || "");
+        setCountry(data.countryName);
+        setCountryCode(getFlagEmoji(data.countryCode));
+      } catch (error) {
+        setGeoCodingError(error.message)
+      } finally {
+        setIsLoadingGeoCoding(false);
+      }
+    }
+
+    fetchLocationData();
+  }, [lat, lng]);
+
+  if (isLoadingGeoCoding) return <p>Loading...</p>;
+  if (geoCodingError) return <p>{geoCodingError}</p>
+
   return (
     <>
       <h2 className="font-bold text-xl text-start">Bookmark New Location</h2>
@@ -13,6 +60,10 @@ function AddNewBookmark() {
             City Name:{" "}
           </label>
           <input
+            value={cityName}
+            onChange={(e) => {
+              e.target.value;
+            }}
             type="text"
             name="cityname"
             id="cityname"
@@ -21,22 +72,27 @@ function AddNewBookmark() {
         </div>
         <div className="flex flex-col items-start gap-2">
           <label htmlFor="cityname" className="text-slate-500">
-            City Name:{" "}
+            Country Name:{" "}
           </label>
           <input
+            value={country}
+            onChange={(e) => {
+              e.target.value;
+            }}
             type="text"
             name="cityname"
             id="cityname"
             className="border-solid border-slate-400 border-[2px] rounded-xl px-3 py-1.5 outline-none font-medium w-full"
           />
+          <span className="absolute bottom-[26rem] left-[31rem]">{countryCode}</span>
         </div>
         <div className="flex flex-row items-start justify-between mt-4">
-          <button 
-          className="border-solid border-blue-500 border-2 px-2 py-1 rounded-xl text-blue-500 hover:bg-blue-500 hover:text-slate-100 w-[6rem] font-medium"
-          onClick={(e) => {
-            e.preventDefault();
-            navigate(-1)
-          }}
+          <button
+            className="border-solid border-blue-500 border-2 px-2 py-1 rounded-xl text-blue-500 hover:bg-blue-500 hover:text-slate-100 w-[6rem] font-medium"
+            onClick={(e) => {
+              e.preventDefault();
+              navigate(-1);
+            }}
           >
             &larr; Back
           </button>
