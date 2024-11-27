61f39098542bcc2b9dd69827d8c44baad4c026eb
Azim Hatami <azimhatami4@gmail.com>
2024-11-27 19:07:56 +0330

    Load async data in user component

--- redux/src/components/UsersList.jsx
+++ redux/src/components/UsersList.jsx
@@ -6,13 +6,29 @@ import { useDispatch, useSelector } from 'react-redux'
 function UsersList() {
 	const dispatch = useDispatch();
 	const state = useSelector(state => state.users);
-	console.log(state)
+
 	useEffect(() => {
 		dispatch(fetchUsers())
 	}, [dispatch])
+
 	return(
 		<>
 			<h2>Users List</h2>
+		{
+			state.loading ? (
+				<p>Loading...</p>
+			) : state.error ? (
+				<p>{state.error}</p>
+			) : (
+				<div>
+					{
+						state.data.map(
+							(user) => <li key={user.id}>{user.name}</li>
+						)
+					}
+				</div>
+			)
+		}
 		</>
 	);
 }
