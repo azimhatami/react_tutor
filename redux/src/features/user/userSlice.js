42b0f9590e84ab3458452f3968e2c551494f3317
Azim Hatami <azimhatami4@gmail.com>
2024-12-07 21:26:13 +0330

    Fix: Updated user slice to use builder callback syntax for extraReducers

--- redux/src/features/user/userSlice.js
+++ redux/src/features/user/userSlice.js
@@ -1,8 +1,9 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 export const getAsyncUsers = createAsyncThunk('user/getAsyncUsers', async (_, { rejectWithValue }) => {
   try {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/users/');
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
     return response.data
   } catch(error) {
     return rejectWithValue(error.message);
@@ -19,25 +20,24 @@ const initialState = {
 const userSlice = createSlice({
   name: 'user',
   initialState,
-  extraReducers: {
-    [getAsyncUsers.pending]: (state, action) => {
-      state.loading = true;
-      state.data = [];
-      state.error = '';
-    },
-    [getAsyncUsers.fullfilled]: (state, action) => {
-      state.data = action.payload;
-      state.loading = false;
-    },
-    [getAsyncUsers.rejected]: (state, action) => {
-      state.loading = false;
-      state.error = action.payload;
-      state.data = [];
-    },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAsyncUsers.pending, (state, action) => {
+        state.loading = true;
+        state.data = [];
+        state.error = '';
+      })
+      .addCase(getAsyncUsers.fulfilled, (state, action) => {
+        state.data = action.payload;
+        state.loading = false;
+      })
+      .addCase(getAsyncUsers.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+        state.data = [];
+      })
   },
 });
 
-
-export const {} = userSlice.actions;
 
 export default userSlice.reducer;
