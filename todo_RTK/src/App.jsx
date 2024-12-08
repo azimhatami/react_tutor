9b3511068ba76945b7a2658b1f93e7e528b2475a
Azim Hatami <azimhatami4@gmail.com>
2024-12-08 12:01:19 +0330

    Initial commit

---
+++ todo_RTK/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2 className='text-blue-500'>Todo RTK</h2>
+    </>
+  )
+}
+
+export default App
